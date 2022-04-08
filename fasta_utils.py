13f9d24d0e8cb12ba6d781bfe62d4bdef40bef9b
Fabian Vogt <fabianxvogt@gmail.com>
2022-04-09 01:05:42 +0200

    fix fasta_to_dict()

--- fasta_utils.py
+++ fasta_utils.py
@@ -5,13 +5,23 @@ from io import TextIOWrapper
 # - fasta_file_path: FASTA file path to read from
 def fasta_to_dict(fasta_file_path: str):
     d = {}
+    k = ""
+    v = []
     with open(fasta_file_path) as f:
         for l in f.read().splitlines():
             if l.startswith(">"):
+                # Merge previous data string
+                if (k != ""):
+                    d[k] = "".join(v)
+                # Set new key
                 k = l[1:]
+                # Init dict
                 d[k] = ""
             else: 
-                d[k] += l
+                v += [l]
+        # Merge last data string
+        if (k != ""):
+            d[k] = "".join(v)
     return d
 
 # Write a record to a file in FASTA format
